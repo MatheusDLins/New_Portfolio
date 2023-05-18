d669a7282c4d868ef01abcd3cd63a394a610a747
MatheusDLins <matheus934qwer@gmail.com>
2023-05-18 20:26:13 -0300

    menu e rodape

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NavBarComponent } from './pages/home/fragments/nav-bar/nav-bar.component';
+import { FooterComponent } from './pages/home/fragments/footer/footer.component';
+import { MainComponent } from './pages/home/fragments/main/main.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NavBarComponent,
+    FooterComponent,
+    MainComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
